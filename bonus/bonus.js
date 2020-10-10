a3c7b9cfc9bbfd1572bcdf916c603d246ee44f6c
Corey Ladovsky <coreyskymusic@gmail.com>
2020-10-09 18:24:27 -0700

    string problems and bonus problems

---
+++ bonus/bonus.js
@@ -0,0 +1,67 @@
+/**
+ * Takes a word and replaces all the vowels with an asterisk.
+ * @param {string}
+ * @returns {string}
+ */
+function replaceVowels() {}
+
+/**
+ * Takes in a string and adds "#" if the string has
+ * an odd number of characters.
+ * @param {string}
+ * @returns {string}
+ */
+function makeStringEvenLength() {}
+
+/**
+ * Takes in a string and returns the last three
+ * letters of the string. If the string is less than
+ * three characters put the code: throw Error("input less than three characters")
+ * @param {string}
+ * @returns {string}
+ */
+
+function lastThreeLetters() {}
+
+/**
+ * Takes in a string and returns whether or not that string starts with a vowel.
+ * @param {string}
+ * @returns {boolean}
+ */
+function doesStartWithVowel() {}
+
+/**
+ * Takes in two strings and returns a new string
+ * with the two strings separated by a space.
+ * @param {string} str1
+ * @param {string} str2
+ * @returns {string}
+ */
+function combineStrings(){}
+/**
+ * Write a function that takes in two strings, `str1` and `str2`,
+ * return a new string with `str2` mashed into the middle of `str1`
+ * Exp str1 = 'help' str2 = 'me' returns 'hemelp.
+ * Exp str1 = 'hello' str2 = 'world' returns 'helworldlo.
+ * @param {string} str1
+ * @param {string} str2
+ * @returns {string}
+ */
+function mashUp(){}
+/**
+ * Takes in a string and returns whether or not is includes a vowel.
+ * Don't use the || (OR) operator,  use the includes method and make it 
+ * case insensitive https://en.wikipedia.org/wiki/Case_sensitivity.
+ * @param {string}
+ * @returns {boolean}
+ */
+function includesVowel(){}
+
+/**
+ * Takes in a string and returns the number of vowels that it contains.
+ * You may use loops for this.
+ * @param {string}
+ * @returns {number} number of vowels
+ */
+
+ function vowelCount(){};
---
+++ problems/strings.js
@@ -0,0 +1,129 @@
+/**
+ * Returns the first character of a string passed in.
+ * @param {string} - str
+ * @returns {string} - first char
+ */
+function firstChar() {}
+
+/**
+ * Returns the length of a string
+ * @param {string} - str
+ * @returns {number} - length of string
+ */
+
+function stringLength() {}
+
+/**
+ * Returns the last character of any string
+ * @param {string} - str
+ * @returns {string} - last letter
+ */
+
+function lastLetter() {}
+
+/**
+ * Takes a single string variable and returns a copy of the string with the
+ * letter capitalized.
+ * @param {string} - str 'jimmy'
+ * @returns {string} - 'jimmY'
+ */
+
+function capitalizeLastLetter() {}
+
+/**
+ *  Create a drEvil function that will take a single number variable, and log the '<variableAmount> dollars',
+your code block should add '(pinky)' at the end of the amount if it's 1 million. For example:
+
+```js
+let amount = 10
+// => 10 dollars
+let amount = 1000000
+// =>  1000000 dollars (pinky)
+```
+* @param {number}
+* returns {string}
+ */
+function drEvil() {}
+
+/**
+ * 
+ * Create a `verbing` function.
+ * It should take a single string variable. 
+ * If its length is at least 3, it should add 'ing' to its end, unless it already ends in 'ing', 
+ * in which case it should add 'ly' instead. If the string length is less than 3, it should leave it unchanged. 
+ * @param {string}
+ * @returns {string}
+For example:
+
+```js
+verbing('box')
+// => 'boxing'
+verbing('train')
+// => 'training'
+verbing('swimming')
+// =>  'swimmingly'
+verbing('go')
+// =>  'go'
+```
+ 
+ */
+
+function verbing() {}
+
+/**
+ * Takes a single string and returns a copy of the string without the
+ * last word.
+ * @param {string}
+ * @returns {string}
+ */
+
+function removeLastWord() {}
+
+/**
+ * Takes a string and returns a new string that has only the even
+ * indexed characters.
+ * Do NOT split the string into an array.
+ * @param {string}
+ * @returns {string}
+ */
+
+function everyOtherLetter() {}
+
+/**
+ * Takes in a string and returns a new string that has all the vowels removed.
+ * @param {string}
+ * @returns {string}
+ */
+ function disemvowel() {}
+
+/**
+ * Takes a string sentence and returns a new sentence. 
+ * Any word that has a length of 5 or more characters should be disemvoweled. 
+ * @param {string}
+ * @returns {string}
+ */
+
+
+ /** 
+  * Takes in a string and returns the middle char. 
+  * If there is no character in the middle return the middle two characters
+  * @param {string}
+  * @returns {string}
+  */
+ function middleCharacter(){}
+
+/**
+ * Takes in a string and reverses it. 
+ * Do not use split 
+ * @param {string}
+ * @returns {string}
+ */
+
+ function reverseString(){}
+
+ /**
+  * Takes in a sentence string and reverses each word but not the 
+  * order of the words. 
+  * @param {string}
+  * @returns {string}
+  */
